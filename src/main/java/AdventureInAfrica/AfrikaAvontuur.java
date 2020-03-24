f79200454a24bc173c38f5674815da81d2296944
Bart de Kinkelaar <bar.kinkelaar@student.han.nl>
2020-03-24 15:33:25 +0100

    Test

---
+++ src/main/java/AdventureInAfrica/AfrikaAvontuur.java
@@ -0,0 +1,32 @@
+package AdventureInAfrica;
+
+import nl.han.ica.oopg.engine.GameEngine;
+import nl.han.ica.oopg.objects.TextObject;
+import nl.han.ica.oopg.view.View;
+
+public class AfrikaAvontuur extends GameEngine {
+	public static void main(String[] args) {
+		AfrikaAvontuur hw = new AfrikaAvontuur();
+
+		// deze methode start de game.
+		hw.runSketch();
+	}
+	//test
+	@Override
+	public void setupGame() {
+		int worldWidth	= 500;
+		int worldHeight	= 500;
+		TextObject to = new TextObject("Hello, World!", 40);
+		to.setForeColor(255, 255, 255, 255);
+		addGameObject(to, 100, 100);
+
+		View view = new View(worldWidth, worldHeight);
+		setView(view);
+		size(worldWidth, worldHeight);
+	}
+
+	@Override
+	public void update() {
+		// Dit doet nog niets }
+	}
+}
