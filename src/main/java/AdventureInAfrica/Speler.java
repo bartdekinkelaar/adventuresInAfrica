c9b2022eb04ee4f9c37e4260de133d06b97af715
Bart de Kinkelaar <bar.kinkelaar@student.han.nl>
2020-03-24 17:01:50 +0100

    Losse plaatjes in apart mapje
    Even de losse plaatjes die ook in Drive staan aan het project toegevoegd.

---
+++ src/main/java/AdventureInAfrica/Speler.java
@@ -0,0 +1,33 @@
+package AdventureInAfrica;
+
+import bart.tutorialworld.TutorialWorld;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.sound.Sound;
+
+public class Speler extends SpriteObject {
+	private AfrikaAvontuur wereld;
+
+	public Speler(AfrikaAvontuur wereld) {
+		// Met `.concat()` plak je 2 strings aan elkaar.
+		// De methode returned een nieuwe String terug.
+//		super(new Sprite(AfrikaAvontuur.MEDIA_URL.concat("vreek.png")));
+		this.wereld = wereld;
+	}
+
+	@Override
+	public void update() {
+
+	}
+
+	public void keyPressed(int keyCode, char key) {
+		final int speed = 5;
+		if (keyCode == world.LEFT) {
+			System.out.println("Ik druk op de links knop");
+			setDirectionSpeed(270, speed);
+		}
+		if (keyCode == world.RIGHT) {
+			System.out.println("Ik druk op de rechts knop");
+			setDirectionSpeed(90, speed);
+		}
+	}
+}
