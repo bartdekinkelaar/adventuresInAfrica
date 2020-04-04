7871c0234ce314410d6e64221177d375edcbbd06
Bart de Kinkelaar <bar.kinkelaar@student.han.nl>
2020-04-04 12:06:12 +0200

    Dave's code added
    Banaan en poep

--- src/main/java/AdventureInAfrica/Banaan.java
+++ src/main/java/AdventureInAfrica/Banaan.java
@@ -1,5 +1,32 @@
 package AdventureInAfrica;
+import java.util.List;
+import nl.han.ica.oopg.collision.ICollidableWithGameObjects;
+import nl.han.ica.oopg.objects.GameObject;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.SpriteObject;
 
-public class Banaan {
+public class Banaan extends SpriteObject implements ICollidableWithGameObjects{
+    private AfrikaAvontuur wereld;
+    private float snelheid;
+    private float richting;
 
+    public Banaan(AfrikaAvontuur wereld) {
+        super(new Sprite(AfrikaAvontuur.MEDIA_URL.concat("banaan.png")));
+        this.wereld = wereld;
+    }
+
+    @Override
+    public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+        for (GameObject go: collidedGameObjects) {
+            if (go instanceof Apen) {
+                wereld.deleteGameObject(this);
+            }
+        }
+    }
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
 }
